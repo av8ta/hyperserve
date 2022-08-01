c243fa8d6e63673e94ece883f18f0fa7c56e642e
av8ta <av8ta@protonmail.com>
2022-08-01 12:55:32 +0800

    implement put and delete - put single blob - put directory recusively by glob pattern - delete hyperdrive url recursively

--- hyperserve.js
+++ hyperserve.js
@@ -5,6 +5,10 @@ import ram from 'random-access-memory'
 import { EventEmitter } from 'events'
 import http from 'http'
 import { defaultHttpHandler } from './http.js'
+import fs from 'node:fs/promises'
+import path from 'path'
+import pull from 'pull-stream'
+import { read } from 'pull-files'
 
 let _corestore, _drive, _swarm
 
@@ -18,6 +22,12 @@ export class Hyperserve extends EventEmitter {
   }
 
   serve = opts => httpServer(_drive, opts)
+
+  put = async (url, filepath, readFileOpts) => await putBlob(_drive, url, await fs.readFile(filepath, readFileOpts))
+  putDir = async (url, directory, glob = '**/*') => await putFiles(_drive, { url, directory, glob })
+
+  delete = async (url, opts = { recursive: true }) => await deleteBlobs(_drive, url, opts)
+  del = async (url, opts = { recursive: true }) => await deleteBlobs(_drive, url, opts)
 
   ready = () => {
     return this.opening
@@ -77,4 +87,48 @@ async function httpServer (drive, { handler = defaultHttpHandler, port = 8080 } 
 
 function isString (s) {
   return !!(typeof s === 'string' || s instanceof String)
+}
+
+async function putBlob (drive, url, blob) {
+  const driveContent = await drive.get(url)
+  // only put blob if it has changed
+  if (compareBlobs(driveContent, blob) !== 0) {
+    await drive.put(url, blob)
+    console.warn('Put blob:', url)
+  } else console.warn('Blob is same, skipping put:', url)
+}
+
+function compareBlobs (left, right) {
+  if (!left || !right) return -1
+  left = Buffer.isBuffer(left) ? left : Buffer.from(left)
+  right = Buffer.isBuffer(right) ? right : Buffer.from(right)
+  return Buffer.compare(left, right)
+}
+
+async function putFiles (drive, { url, directory, glob }) {
+  return new Promise((resolve, reject) => {
+    pull(
+      read(path.join(directory, glob)),
+      pull.asyncMap(async (file, callback) => {
+        const dest = path.join(url, file.path)
+        console.warn('Writing to hyperdrive:', dest)
+        await putBlob(drive, dest, file.data)
+        callback(null, dest)
+      }),
+      pull.collect((error, urls) => {
+        if (error) reject(error)
+        resolve(urls)
+      })
+    )
+  })
+}
+
+async function deleteBlobs (drive, url, options) {
+  const urls = []
+  for await (const { key } of drive.list(url, options)) {
+    console.log('Deleting from hyperdrive:', key)
+    await drive.del(key)
+    urls.push(key)
+  }
+  return urls
 }
